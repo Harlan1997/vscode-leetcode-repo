30cd88adb2be967ff31d883a6de32576394d0814
liangmiao <miaoliang@chinaesport.com>
2022-01-05 21:53:05 +0800

    41

--- test.cpp
+++ test.cpp
@@ -4,32 +4,34 @@ public:
     vector<vector<int>> res;
     vector<int> tmp;
 
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         backTracking(candidates, target, 0, tmp);
         return res;
     }
 
     void backTracking(vector<int>& candidates, int target, int index, vector<int> &tmp) {
-        if (index == candidates.size()) {
-            return;
-        }
         if (target == 0) {
             res.push_back(tmp);
             return;
         } 
-        //选中当前值
-        backTracking(candidates, target, index + 1, tmp);
-        //不选中当前值
-        if (target - candidates[index] >= 0) {
-            tmp.push_back(candidates[index]);
-            backTracking(candidates, target - candidates[index], index, tmp);
-            tmp.pop_back();
+        for(int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            if (target - candidates[i] >= 0) {
+                tmp.push_back(candidates[i]);
+                backTracking(candidates, target - candidates[i], i + 1, tmp);
+                tmp.pop_back();
+            } else {
+                break;
+            }
         }
     }
 };
 
 int main() {
     Solution *s = new Solution();
-    vector<int> v = {2, 7, 6, 3, 5, 1};
-    cout << s->combinationSum(v, 9).size();
+    vector<int> v = {2, 5, 2, 1, 2};
+    cout << s->combinationSum2(v, 5).size();
 }
